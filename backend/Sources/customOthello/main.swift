8e9ef2a3e121b0a4fab11f4e9c27e7d651967879
Koji Aomatsu <penbook.koji@gmail.com>
2017-09-04 11:48:38 +0900

    Enable to response json

--- backend/Sources/customOthello/main.swift
+++ backend/Sources/customOthello/main.swift
@@ -3,6 +3,7 @@ import KituraNet
 import HeliumLogger
 import Mustache
 import Foundation
+import SwiftyJSON
 
 HeliumLogger.use()
 
@@ -19,6 +20,28 @@ router.get("/") {
         let html = try! template.render(with: Box(data))
         response.status(HTTPStatusCode.OK).send(html)
         next()
+}
+
+router.get("/list") {
+    request, response, next in
+        let dict : [String: Any] = [
+            "length": 2,
+            "list" : [
+                [
+                "id" : 3,
+                "size" : 6,
+                "ppt" : 2
+                ],
+                [
+                "id" : 4,
+                "size" : 8,
+                "ppt" : 1
+                ],
+            ]
+        ]
+        response.headers["Content-Type"] = "application/json; charset=UTF-8"
+        try response.send(json: JSON(dict)).end()
+
 }
 
 Kitura.addHTTPServer(onPort: 8080, with: router)
